bc7a2f83a4af5d25db184bca6f389723a8e6f246
jojosrepositories <sharkboyjrh@gmail.com>
2026-03-07 12:03:12 -0600

    main.cpp

--- jojosSandbox/jojosSandbox/main.cpp
+++ jojosSandbox/jojosSandbox/main.cpp
@@ -4,10 +4,13 @@
 
 int main()
 {
+	// 3 examples of class
 	aquaTeenCharacters aquaTeenCharacter1("masterShake", "milkshake", true);
 	aquaTeenCharacters aquaTeenCharacter2("fryLoc", "french-fry", false);
 	aquaTeenCharacters aquaTeenCharacter3("meatWad", "ground meat", false);
 
+	// out-puting using setters & getters
+	// uses boolaplha for an easier read t/f > 1/2
 	std::cout << std::boolalpha;
 	std::cout << aquaTeenCharacter1.getName() << std::endl;
 	std::cout << aquaTeenCharacter1.getRace() << std::endl;
@@ -25,4 +28,5 @@ int main()
 	std::cout << aquaTeenCharacter3.getRace() << std::endl;
 	std::cout << "are they a detective?" << " " << aquaTeenCharacter3.getDetective() << std::endl;
 	std::cout << " " << std::endl;
+
 }
